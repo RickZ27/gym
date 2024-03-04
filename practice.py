395e73448b3f7a3a4add674333d577f10a2da064
RickZ27 <umanganayaju67@gmail.com>
2024-03-05 00:07:26 +0545

    login and registration cmplete

---
+++ practice.py
@@ -0,0 +1,53 @@
+# a="capital"
+# s=a.center(11, '*')
+# print(s)
+
+# a="python"
+# print(a.center(11,"*"))
+
+# print(~19)
+
+# x = {"python","java","c"}
+# y = {"google","java"}
+# z = x.symmetric_difference(y)
+# print(z)
+
+# fruits = (1,2,3,4,5,4,4)
+# fruits[0]="python"
+# print(fruits)
+
+# fruits = [1, 2, 3, 4, 5, 4, 4]
+# fruits[0] = "python"
+# print(fruits)
+
+# car = {"brand": "Ford", "model": "Mustang","year": 1964}
+# c=car.popitem()
+# print(c)
+
+# a="python"
+# sum=3
+# for i in range(1,8):
+#     sum=sum+i+2
+# print(sum)
+
+# print("Hello PYthon".istitle())
+
+# d={}
+# d=[100]='python'
+# d=[200]='programming'
+# print(d)
+
+# age=input('enter age:')
+# f=open('data.txt', 'w')
+# f.write(age)
+# f.close()
+
+# f=open('filehandeling.txt')
+# if f:
+#     print('file open')
+
+l=[1,2]
+print(l*2)
+
+
+
